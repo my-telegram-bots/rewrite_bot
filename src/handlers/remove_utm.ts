8b09a4bd08d5c273f85da657538b6d67f7f087b4
huggy <i@huggy.moe>
2022-03-29 15:02:36 +0800

    feat: add(modify) domainPathParamsList improve `remove UTM` function

--- src/handlers/remove_utm.ts
+++ src/handlers/remove_utm.ts
@@ -1,28 +1,46 @@
 import axios from 'axios'
 import { REDIRECT_CHECK_API } from '../config'
+import { domainPathParamsList, domainPathUrlReplaceList } from '../schema'
 // utm_* see wiki https://en.wikipedia.org/wiki/UTM_parameters
-const utm_params = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content', 'fbclid']
-// need help
-// Element implicitly has an 'any' type because expression of type 'string' can't be used to index type '{ "www.bilibili.com": string[]; }'.
-const hostname_utm_params_blacklist = {
+const utm_params = {
+    '/': ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content', 'fbclid']
+}
+// maybe need wildcard support (match */xxx/)
+const hostname_utm_params_blacklist: domainPathParamsList = {
     // www = @
-    'www.bilibili.com': ['share_medium', 'share_plat', 'share_session_id', 'share_source', 'share_tag', 'timestamp', 'unique_k'],
-    'www.twitter.com': ['t', 's'],
-    'mobile.twitter.com': ['t', 's']
+    ['www.bilibili.com']: {
+        '/': ['share_medium', 'share_plat', 'share_session_id', 'share_source', 'share_tag', 'timestamp', 'unique_k']
+    },
+    'www.twitter.com': {
+        '/': ['t', 's']
+    }
 }
-const hostname_utm_params_whitelist = {
-    'item.taobao.com': ['id'],
-    'a.m.taobao.com': [],
-    'detail.tmall.com': ['id']
+const hostname_utm_params_whitelist: domainPathParamsList = {
+    'www.bilibili.com': {
+        //               t=time
+        '/video/': ['p', 't']
+    },
+    'item.taobao.com': {
+        '/': ['id']
+    },
+    // :id.html
+    'a.m.taobao.com': {
+        '/': []
+    },
+    'detail.tmall.com': {
+        '/': ['id']
+    }
 }
-const hostname_replace = {
-
+const hostname_map = {
+    'mobile.twitter.com': 'www.twitter.com',
 }
 // 感觉很侵入性，后面有机会再来优化了 ~~出锅以后再说~~
-const hostname_url_replace = {
-    'www.bilibili.com': [['?p=1', '']],
+const hostname_url_replace: domainPathUrlReplaceList = {
+    'www.bilibili.com': {
+        '/video/': [['?p=1', '']],
+    }
 }
-const short_url_service_domain = ['g.co', 'aka.ms', 'amazon.to', 't.co', 'u.nu', 'bit.ly', 'tinyurl.com', 't.cn', 'b23.tv']
+const short_url_service_domain = ['g.co', 'aka.ms', 'amazon.to', 't.co', 'u.nu', 'bit.ly', 'tinyurl.com', 'b23.tv']
 
 export async function get_redirect(url = '', retry_time = 0): Promise<string> {
     if (retry_time < 5) {
@@ -70,50 +88,75 @@ export async function real_remove_utm(url = ''): Promise<string> {
         const u = new URL(url)
         const uu = u.searchParams
         let hostname: string = u.hostname
+        if (short_url_service_domain.includes(hostname)) {
+            return await real_remove_utm(await get_redirect(url))
+        }
         // www = @
         if (hostname.split('.').length === 2) {
             hostname = 'www.' + hostname
         }
-        // @ts-ignore
+        //             bad
+        let whitelist: any = []
         if (hostname_utm_params_whitelist[hostname]) {
+            for (const key in hostname_utm_params_whitelist[hostname]) {
+                if (u.pathname.startsWith(key)) {
+                    // whitelist = [...whitelist, hostname_utm_params_whitelist[hostname][key]]
+                    whitelist = hostname_utm_params_whitelist[hostname][key]
+                    // match to end
+                    // break
+                }
+            }
+        }
+        if (whitelist.length > 0) {
             // rm all params
             const wu = new URL(url.replace(u.search, ''))
             const wuu = wu.searchParams
-            // @ts-ignore
-            hostname_utm_params_whitelist[hostname].forEach((p: string) => {
+            whitelist.forEach((p: string) => {
                 if (uu.get(p)) {
                     wuu.set(p, <string>uu.get(p))
                 }
             })
             url = wu.href
-        } else {
-            let rm_params = utm_params
-            // @ts-ignore
-            if (hostname_utm_params_blacklist[hostname]) {
-                // @ts-ignore
-                rm_params = [...rm_params, ...hostname_utm_params_blacklist[hostname]]
-            }
-            rm_params.forEach((p) => {
-                uu.delete(p)
-            })
-            if (short_url_service_domain.includes(u.hostname)) {
-                return await real_remove_utm(await get_redirect(url))
-            }
-            // @ts-ignore
-            hostname_url_replace[hostname].forEach(([u1, u2]) => {
-                u.href = u.href.replace(u1, u2)
-            })
-            url = u.href
         }
+        let blacklist: any[] = utm_params['/']
+
+        if (hostname_utm_params_blacklist[hostname]) {
+            let temp_blacklist: any = []
+            for (const key in hostname_utm_params_blacklist[hostname]) {
+                if (u.pathname.startsWith(key)) {
+                    // blacklist = [...blacklist, hostname_utm_params_blacklist[hostname][key]]
+                    temp_blacklist = hostname_utm_params_blacklist[hostname][key]
+                    // match to end
+                    // break
+                }
+            }
+            if (temp_blacklist.length > 0) {
+                blacklist = [...temp_blacklist, ...blacklist]
+            }
+        }
+        blacklist.forEach((p) => {
+            uu.delete(p)
+        })
+        let url_replace_list: Array<[string, string]> = []
+        for (const key in hostname_url_replace[hostname]) {
+            if (u.pathname.startsWith(key)) {
+                url_replace_list = hostname_url_replace[hostname][key]
+                // break
+            }
+        }
+        url_replace_list.forEach(([u1, u2]) => {
+            u.href = u.href.replace(u1, u2)
+        })
+        url = u.href
     } catch (error) {
     }
     return url
 }
 export default async (text = ''): Promise<string> => {
     let stext = await Promise.all(text.split('\n').map(async (l) => {
-        return (await Promise.all(l.split('http').map(async (l) => {
-            return await real_remove_utm(l)
-        }))).join('\n')
+        return (await Promise.all(l.split(' ').map(async (l) => {
+            return (await real_remove_utm(l))
+        }))).join(' ')
     }))
     return stext.join('\n')
 }
--- src/schema.ts
+++ src/schema.ts
@@ -3,3 +3,14 @@ export interface ThideText {
     mode: 'inline' | 'message',
     type: number
 }
+
+export interface domainPathParamsList {
+    [key: string]: {
+        [key: string]: Array<string>
+    }
+}
+export interface domainPathUrlReplaceList {
+    [key: string]: {
+        [key: string]: Array<[string, string]>
+    }
+}
